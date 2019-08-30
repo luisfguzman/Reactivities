f8fcdaafdd7ea9a8b579ac5031bb88f54573249e
CN\162976 <luisfelipe.guzman@cn.ca>
2019-08-30 12:00:13 -0400

    Added MobX for state management in client-ap

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,20 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProps {
-  activity: IActivity | null;
-  setEditMode: (editMode: boolean) => void;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
+  activity: IActivity | undefined;
 }
 
 const ActivityForm: React.FC<IProps> = ({
-  activity: initialFormState,
-  setEditMode,
-  createActivity,
-  editActivity,
-  submitting
+  activity: initialFormState
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const { createActivity, editActivity, submitting, cancelFormOpen } = activityStore;
+
   const initializeForm = () => {
     if (initialFormState) return initialFormState;
     else
@@ -95,7 +92,7 @@ const ActivityForm: React.FC<IProps> = ({
           value={activity.venue}
         />
         <Button
-          onClick={() => setEditMode(false)}
+          onClick={cancelFormOpen}
           floated="right"
           type="button"
           content="Cancel"
@@ -112,4 +109,4 @@ const ActivityForm: React.FC<IProps> = ({
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
