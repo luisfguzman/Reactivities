dd05ea20ca4be5bbe9ca4a09f4c6b9361787d4d6
CN\162976 <luisfelipe.guzman@cn.ca>
2019-09-19 10:56:22 -0400

    Added Authorization to the backend part 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,11 @@ using Domain;
 using Microsoft.AspNetCore.Identity;
 using Application.Interfaces;
 using Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -37,7 +42,10 @@ namespace API
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddMvc()
+            services.AddMvc(opt => {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
                 .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Create>())
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             
@@ -46,7 +54,20 @@ namespace API
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt => {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false
+                    };
+                });
+
             services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +85,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseCors("CorsPolicy");
             app.UseMvc();
         }
---
+++ Application/Validators/ValidatorExtensions.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public static class ValidatorExtensions
+    {
+        public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var options = ruleBuilder
+                .NotEmpty()
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+                .Matches("[A-Z]").WithMessage("Password must contain at least 1 upper case letter.")
+                .Matches("[a-z]").WithMessage("Password must contain at least 1 lower case letter.")
+                .Matches("[0-9]").WithMessage("Password must contain at least 1 number.")
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least 1 alphanumeric.");
+            return options;
+        }
+    }
+}
